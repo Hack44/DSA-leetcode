596a64e4ed50b55ee1e0507dbe34584bfcb28db1
Harsh Verma <royalharsh2102@gmail.com>
2026-09-21 18:10:50 +0530

    Time: 127 ms (21.87%) | Memory: 237.2 MB (14.75%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,25 +1,28 @@
 class Solution {
     int [][] dp;
     public int lengthOfLIS(int[] nums) {
-        dp = new int[nums.length][nums.length+1];
+        dp= new int[nums.length][nums.length+1];
         for(int i=0; i<nums.length; i++){
-            Arrays.fill(dp[i], -1);
+            Arrays.fill(dp[i],-1);
         }
-        return func(nums, 0, -1, dp);
+        return func(nums, 0, -1,dp);
     }
-    int func(int [] nums, int i, int p, int [][] dp){
+    int func(int [] nums, int i,int p, int[][] dp){
         if(i>= nums.length){
             return 0;
-        }  
-        if(dp[i][p +1]!= -1){
-            return dp[i][p +1];
-        }     
-        int a=0;
-        if( p== -1 || nums[i] > nums[p]){
-          a = 1+ func( nums, i+1, i, dp);
+        } 
+        if(dp[i][p+1]!= -1){
+            return dp[i][p+1];
         }
-        int b= func(nums, i+1, p, dp);
-        dp[i][p +1]= Math.max(a,b);
+           
+        int M=0;
+        for(int j=i; j<nums.length; j++){
+            if( p== -1 || nums[j] > nums[p]){
+            int a = 1+ func( nums, j+1, j,dp);
+             M = Math.max(M,a);
+         }
+        }
+        dp[i][p+1] =M;
         return dp[i][p+1];
     }
     
