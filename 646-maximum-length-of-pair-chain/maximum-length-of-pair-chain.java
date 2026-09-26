d168851485062ff4ad1087b1bd28101afed340f1
Harsh Verma <royalharsh2102@gmail.com>
2026-09-26 09:29:20 +0530

    Time: 35 ms (18.17%) | Memory: 65.6 MB (12.73%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.java
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.java
@@ -1,30 +1,41 @@
 class Solution {
     int [][] dp;
     public int findLongestChain(int[][] pairs) {
-        dp = new int[ pairs.length][pairs.length+1];
+        dp = new int[ pairs.length][pairs.length];
+
         Arrays.sort(pairs, (a, b) -> a[0] - b[0]);
+
         for(int i=0; i< pairs.length; i++){
             Arrays.fill(dp[i], -1);
         }
-        int ans = func(pairs, 0, -1, dp);
+        int ans = func(pairs, 0, dp);
         return ans;
     }
-    public int func(int [][] pairs, int i, int prev, int [][] dp){
+    public int func(int [][] pairs, int i, int [][] dp){
        
         if(i>= pairs.length){
             return 0;
         }
-        if(dp[i][prev+1] != -1){
-            return dp[i][prev+1];
+        if(dp[i][0] != -1){
+            return dp[i][0];
         }
-        int take =0;
-        if(prev == -1 || pairs[prev][1] < pairs[i][0]){
-            take = 1 + func(pairs, i+1, i,dp);
-            
+       int id = pairs.length;
+        for(int j= i+1; j< pairs.length; j++){
+            if(pairs[i][1] < pairs[j][0]){
+                id= j;
+                break;
+            }   
+
         }
-        int skip = func(pairs, i + 1, prev, dp);
-        dp[i][prev+1]= Math.max(take, skip);
-        return dp[i][prev+1];
+        int take =1;
+        if(id < pairs.length){
+            take = 1 + func(pairs, id,dp);
+        }
+        
+        int skip = func(pairs, i + 1, dp);
+
+        dp[i][0]= Math.max(take, skip);
+        return dp[i][0];
     }
 
 }
